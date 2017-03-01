cfbdbec76cc441b60d5402c18f6b4e21d0ebaf71
kevinhuangg <email.kevinh@gmail.com>
2017-02-28 19:57:12 -0800

    refactored and passed more tests

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,17 +11,19 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
-
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
+  'access-control-max-age': 10, // Seconds.
+  'Content-Type': 'application/json'
 };
 
 var results = []; //storage array for passed in data
+var responseBody = {};
 
 var requestHandler = function(request, response) {
+
 
   //if request.method is equal to 'GET' and request.url is equal to '/classes/messages'
 
@@ -33,62 +35,49 @@ var requestHandler = function(request, response) {
   // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
   // console.log(headers);
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'application/json';
+
+  request.on('error', function(err) {
+    console.error('something wrong happened');
+    response.statusCode = 404;
+    response.end();
+  });
+  request.on('data', function(chunk) { //ask why chaining fails our tests
+    console.log(chunk);
+    if (chunk) {
+      results.push(chunk.toString());
+    }
+  });      
+  // request.on('end', function() { //ask what the fuck this does
+
+  //   response.end();
+  // });
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
   
   //{'Content-Type': 'application/json'}
 
-  if (request.method === 'GET' && request.url === '/classes/messages') {
-    request.on('error', function(err) {
-      console.error('something wrong happened');
-      response.end();
-    });
+  if (request.method === 'GET') {
+    response.statusCode = statusCode;
+    response.writeHead(statusCode, headers);
 
-    request.on('end', function() {
-      response.statusCode = statusCode;
-      response.writeHead(statusCode, headers);
-
-      var responseBody = {
-        method: request.method,
-        // ended: true,
-        url: request.url,
-        headers: headers,
-        responseCode: statusCode,
-        results: results
-      };
-
-      console.log(results);
-
-      response.end(JSON.stringify(responseBody));
-
-    });
-  } 
-
-  if (request.method === 'POST' && request.url === '/classes/messages') {
+    responseBody = {
+      method: request.method,
+      url: request.url,
+      headers: headers,
+      responseCode: statusCode,
+      results: results
+    };
+    // console.log(responseBody.results);
+    // response.end(JSON.stringify(responseBody));
+    
+  } else if (request.method === 'POST') {
     statusCode = 201;
     response.writeHead(statusCode, headers);    
     
-    request.on('error', function(err) {
-      console.error('something wrong happened');
-      response.end();
-    });
-    request.on('data', function(chunk) {
-      console.log(chunk.toString());
-      results.push(chunk.toString());
-    });      
-    request.on('end', function() {
-
-      response.end();
-    });
   }
 
-
+  response.end(JSON.stringify(responseBody));
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
   // response.end() will be the body of the response - i.e. what shows
